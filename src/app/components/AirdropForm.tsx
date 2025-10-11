ab7774d7ad3b197b0b318c9ad033ef2267712ecd
wemmy6673 <okunolawemzy@gmail.com>
2025-10-11 15:31:37 +0100

    contract reading

--- src/app/components/AirdropForm.tsx
+++ src/app/components/AirdropForm.tsx
@@ -2,13 +2,16 @@
 import InputField from "./ui/InputField"
 import { useState } from "react"
 import {chainsToTSender, tsenderAbi, erc20Abi} from "@/constants"
-import { useChainId } from 'wagmi'
+import { useChainId, useConfig, useAccount } from 'wagmi'
+import { readContract } from '@wagmi/core'
 
 export default function AirdropForm() {
     const [tokenAddress, setTokenAddress] = useState("")
     const [recipients, setRecipients] = useState("")
     const [amounts, setAmounts] = useState("")
     const chainId = useChainId()
+    const config = useConfig()
+    const account = useAccount()
 
     async function getApprovedAmount(tSenderAddress: string | null) : Promise<number>{
 
@@ -16,11 +19,20 @@ export default function AirdropForm() {
             alert("No address found, Please use a supported chain")
             return 0
         }
+        const response = await readContract(config, {
+            abi: erc20Abi,
+            address: tokenAddress as `0x${string}`,
+            functionName: 'allowance',
+            args: [account.address, tSenderAddress as `0x${string}`]
+
+        })
+        return response as number
     }
 
 
     async function handleSubmit() {
-        const tsenderAddress = chainsToTSender[chainId]["tsender"]
+        const tSenderAddress = chainsToTSender[chainId]["tsender"]
+        const approvedAmount = await getApprovedAmount(tSenderAddress)
     }
 
     return(
