1d362f85d9c1b8f0c7d4255b7247fd1f2f706e96
wemmy6673 <okunolawemzy@gmail.com>
2025-10-11 21:05:03 +0100

    token total calculation

---
+++ src/utils/calculateTotal.ts
@@ -0,0 +1,16 @@
+function calculateTotal(amounts: string): number {
+    const amountArray = amounts
+    .split(/[\n,]+/)
+    .map(amt => amt.trim())
+    .filter(amt => amt !== "")
+    .map(amt => parseFloat(amt));
+
+    // if(amountArray.some(isNaN)){
+    //     return 0
+
+    // }
+
+    return amountArray
+    .filter(num => !isNaN(num))
+    .reduce((sum, num) => sum + num, 0)
+}
